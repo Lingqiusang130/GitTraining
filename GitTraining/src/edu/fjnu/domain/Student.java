0095d8a8758dbb2af4ffb257ddfbbf23402fd677
Lingqiusang130 <137590332@qq.com>
2014-12-18 01:19:46 +0800

    初始化

---
+++ GitTraining/src/edu/fjnu/domain/Student.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package edu.fjnu.domain;
+
+/**
+ * @author lingqiusang
+ * 
+ */
+public class Student {
+
+	/** 用户名 */
+	private String username;
+	/** 密码 */
+	private String password;
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
