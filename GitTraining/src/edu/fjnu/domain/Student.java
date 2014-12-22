b343573c1cb47bc8ae96dd7211c24e9926df77c1
Lingqiusang130 <137590332@qq.com>
2014-12-22 19:42:42 +0800

    修改

--- GitTraining/src/edu/fjnu/domain/Student.java
+++ GitTraining/src/edu/fjnu/domain/Student.java
@@ -12,7 +12,7 @@ public class Student {
 	/** 用户名 */
 	private String username;
 	/** 密码 */
-	private String pwd;
+	private String pwd1;
 
 	public Student() {
 		super();
@@ -22,7 +22,7 @@ public class Student {
 	public Student(String username, String password) {
 		super();
 		this.username = username;
-		this.pwd = password;
+		this.pwd1 = password;
 	}
 
 	public String getUsername() {
@@ -33,13 +33,15 @@ public class Student {
 		this.username = username;
 	}
 
-	public String getPwd() {
-		return pwd;
+	public String getPwd1() {
+		return pwd1;
 	}
 
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
+	public void setPwd1(String pwd1) {
+		this.pwd1 = pwd1;
 	}
+
+
 
 
 
